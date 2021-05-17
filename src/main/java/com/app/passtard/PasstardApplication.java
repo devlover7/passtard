1180a2d4b9ff4dbac629206862f8ef3a6fda5010
abhinav <abhinavgr007@hotmail.com>
2021-05-17 05:57:16 +0530

    first commit

---
+++ src/main/java/com/app/passtard/PasstardApplication.java
@@ -0,0 +1,13 @@
+package com.app.passtard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasstardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasstardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/passtard/PasstardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.passtard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasstardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
